e5969f2a402a86fbfd34de16da08c006bf2e40e8
Noronha <lab.noronha@gmail.com>
2024-05-15 00:15:09 -0300

    Introduction to programming commit

---
+++ miscellaneous/do_nothing.py
@@ -0,0 +1,6 @@
+#using the pass keyword for the first time
+
+for x in range(3):
+    pass
+
+print("nothing has been Done")
---
+++ miscellaneous/printing.py
@@ -0,0 +1,8 @@
+#playing with the print function
+
+greeting = ""
+
+for char in "Hello World":
+    greeting += char.upper()
+
+print(greeting)
